515a7816412f88dddeeb15fe9c48e5b8ec3ee7fa
cezarpauna <42316548+cezarpauna@users.noreply.github.com>
2025-01-15 21:00:48 +0200

    Add Listings component

--- Listings.js
+++ Listings.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const Listings = () => {
-  const properties = [
-    { id: 1, title: 'Luxury Apartment', price: '$1,200,000' },
-    { id: 2, title: 'Cozy Cottage', price: '$350,000' },
-    { id: 3, title: 'Modern House', price: '$750,000' }
-  ];
+    const properties = [
+        { id: 1, title: 'Beautiful Family Home', price: '$300,000' },
+        { id: 2, title: 'Modern Apartment', price: '$250,000' },
+        { id: 3, title: 'Luxurious Villa', price: '$500,000' },
+    ];
 
-  return (
-    <div>
-      <h2>Property Listings</h2>
-      <ul>
-        {properties.map(property => (
-          <li key={property.id}>{property.title} - {property.price}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Property Listings</h2>
+            <ul>
+                {properties.map(property => (
+                    <li key={property.id}>{property.title} - {property.price}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Listings;
