edda9510c4bf8b82aafd75d66f5b6c149c459f92
cezarpauna <42316548+cezarpauna@users.noreply.github.com>
2025-01-15 21:16:00 +0200

    Added Listings component

--- Listings.js
+++ Listings.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Listings = () => {
   const properties = [
-    { id: 1, title: 'Luxury Apartment', price: '$1,200,000' },
-    { id: 2, title: 'Cozy Cottage', price: '$350,000' },
-    { id: 3, title: 'Modern Villa', price: '$2,500,000' },
+    { id: 1, title: 'Beautiful Family Home', price: 300000},
+    { id: 2, title: 'Modern Apartment', price: 200000},
+    { id: 3, title: 'Charming Cottage', price: 150000}
   ];
 
   return (
@@ -12,7 +12,7 @@ const Listings = () => {
       <h2>Property Listings</h2>
       <ul>
         {properties.map(property => (
-          <li key={property.id}>{property.title} - {property.price}</li>
+          <li key={property.id}>{property.title} - ${property.price}</li>
         ))}
       </ul>
     </div>
